f9459d775975e6536964aee797d2e0b9871fc946
isaacg <iwanyoike65@gmail.com>
2023-03-24 14:14:04 +0300

    Add loading and error control functions

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,11 +3,16 @@ import React, { useState, useEffect } from 'react';
 // fetch data from https://api.api-ninjas.com/v1/quotes?category=success api using fetch
 function Quote() {
   const [quote, setQuote] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
+    const abortController = new AbortController();
 
     const fetchData = async () => {
       try {
+        const abortController = new AbortController();
+        setLoading(true);
         const response = await fetch(
           'https://api.api-ninjas.com/v1/quotes?category=success',
           {
@@ -15,23 +20,31 @@ function Quote() {
             headers: {
               'X-Api-Key': 'diF2CPTnfXZ3z+eeylqvtg==6y2p5KUk1d9qZPsk',
             },
+            signal: abortController.signal,
           },
         );
 
         const data = await response.json();
         setQuote(data[0].quote);
+        setLoading(false);
       } catch (error) {
+        setHasError(true);
       }
     };
 
     fetchData();
 
+    return () => {
+      abortController.abort();
+    };
   }, []);
 
   return (
     <div className="quote">
       <h1>Quote of the day</h1>
-      <p>{quote}</p>
+      <p>{ quote }</p>
+      { isLoading ? <p>Loading...</p> : null }
+      { hasError ? <p>Something went wrong</p> : null }
     </div>
   );
 }
