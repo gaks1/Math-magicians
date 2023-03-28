f51d1a654250e8d027574172d08d48bc99208704
isaacg <iwanyoike65@gmail.com>
2023-03-28 14:00:03 +0300

    try

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
 import Quote from './components/Quote';
 
 function App() {
   return (
     <>
-      <Calculator />
-      <Quote />
+      <h1>Math Magicians</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/calculator">Calculator</Link></li>
+          <li><Link to="/quote">Quote</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+const Home = () => (
+  <div className="home">
+    <h2>Home</h2>
+  </div>
+);
+
+export default Home;
